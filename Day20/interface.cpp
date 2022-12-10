1b94868ee7555bbaa89f09bef8a9707a88c6d69d
sumit jat <jat.sumit0056@gmail.com>
2022-12-11 00:00:09 +0530

    change

--- Day20/interface.cpp
+++ Day20/interface.cpp
@@ -4,28 +4,37 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <math.h>
 using namespace std;
-class AdvancedArithmetic{
-    public:
-        virtual int divisorSum(int n)=0;
-};
-class Calculator : public AdvancedArithmetic {
+class AdvancedArithmetic
+{
 public:
-    int divisorSum(int n) {
-        int sum=0;
-   for (int i = 1; i <= n; i++) {
-  if (n % i == 0){
-        sum+=i;}
-   }
+    virtual int divisorSum(int n) = 0;
+};
+class Calculator : public AdvancedArithmetic
+{
+public:
+    int divisorSum(int n)
+    {
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                sum += i;
+            }
+        }
         return sum;
     }
 };
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
-    AdvancedArithmetic *myCalculator = new Calculator(); 
+    AdvancedArithmetic *myCalculator = new Calculator();
     int sum = myCalculator->divisorSum(n);
-    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    cout << "I implemented: AdvancedArithmetic\n"
+         << sum;
     return 0;
 }
